eff17e3c026e6abb9cf6b0eac3358fd86d63e8e4
muhmdrezki <muhmdrezki@gmail.com>
2022-03-07 23:49:43 +0700

    fix param request

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -88,7 +88,7 @@ class GuestController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($request)
+    public function show(Request $request)
     {
         $txt = file_get_contents('https://www.eenvito.com/backend/public/api/'.$request->type.'.txt');
         return response($txt, 200)->headers('Content-Type', 'text/html');
